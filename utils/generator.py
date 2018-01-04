19e813f33ab0950ed841727f51676225aa3750bb
kHRYSTAL <723526676@qq.com>
2018-01-04 18:11:30 +0800

    add fake password generate method

--- utils/generator.py
+++ utils/generator.py
@@ -47,24 +47,36 @@ def random_str(min_chars=0, max_chars=8):
     return fake.pystr(min_chars=min_chars, max_chars=max_chars)
 
 
+from random import choice
+import string
+
+
+def random_password(length=8, chars=string.ascii_letters + string.digits):
+    return ''.join([choice(chars) for i in range(length)])
+
+
 def factory_generate_ids(starting_id=1, increment=1):
     """ 返回一个生成器函数，调用这个函数产生生成器，从starting_id开始，步长为increment。 """
+
     def generate_started_ids():
         val = starting_id
         local_increment = increment
         while True:
             yield val
             val += local_increment
+
     return generate_started_ids
 
 
 def factory_choice_generator(values):
     """ 返回一个生成器函数，调用这个函数产生生成器，从给定的list中随机取一项。 """
+
     def choice_generator():
         my_list = list(values)
         rand = random.Random()
         while True:
             yield random.choice(my_list)
+
     return choice_generator
 
 
@@ -83,3 +95,4 @@ if __name__ == '__main__':
     choice_gen = factory_choice_generator(choices)()
     for i in range(5):
         print(next(choice_gen))
+    print(random_password(8))
